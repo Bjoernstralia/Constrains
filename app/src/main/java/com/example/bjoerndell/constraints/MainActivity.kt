4fd5b2ed8d25f4c9323b206eda1837fa87651925
Bjoernstralia <B.Viehmann@gmx.de>
2018-04-02 18:04:01 +0200

    Login window

--- app/src/main/java/com/example/bjoerndell/constraints/MainActivity.kt
+++ app/src/main/java/com/example/bjoerndell/constraints/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.bjoerndell.constraints
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTest.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
     }
 }
